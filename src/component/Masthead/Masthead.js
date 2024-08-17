49a5ee2ae1502ca5b372994d9ed833432e3bce19
alfred <narutoalf@yahoo.com>
2024-08-17 17:19:16 +0800

    [Feature] updated masthead to include download button, and more information

--- src/component/Masthead/Masthead.js
+++ src/component/Masthead/Masthead.js
@@ -1,16 +1,32 @@
 import memoji from "../../files/memoji/sticker02.png";
 import styles from "./Masthead.module.css";
 
-import { Grid, Stack, Typography, Link, Button } from "@mui/material";
+import DownloadIcon from "@mui/icons-material/Download";
+import { Button, Grid, Stack, Typography } from "@mui/material";
 
 function Masthead() {
   const date = new Date();
   const year = date.getFullYear();
 
-  const frontendSkills = ["HTML", "CSS", "React.js", "Electron.js"];
+  const frontendSkills = ["HTML", "CSS", "Javascript", "Python"];
   const backendSkills = ["Node.js", "Express.js", "MySQL"];
-  const devopsSkills = ["Docker", "Git", "Gitlab"];
+  const devopsSkills = ["Docker", "Git", "Gitlab", "Kubernetes"];
+  const certificate = ["AWS Solutions Architect - Associate"];
 
+  const downloadResume = async () => {
+    const file = await fetch("https://raw.githubusercontent.com/copipastaguy/resume/main/cv.pdf");
+
+    const blob = await file.blob();
+
+    const downloadURL = window.URL.createObjectURL(blob);
+    const link = document.createElement("a");
+    link.href = downloadURL;
+    link.setAttribute("download", "Alfred_Chor_CV");
+    document.body.appendChild(link);
+
+    link.click();
+    link.remove();
+  };
   return (
     <Grid container height="100vh" alignItems="center" justifyContent="center" spacing={2}>
       <Grid item>
@@ -19,42 +35,41 @@ function Masthead() {
 
       <Stack spacing={2}>
         <Grid item>
-          <Typography variant="h2" fontWeight={400} fontFamily="monospace">
-            console.log&#40;&#34;I'm Alfred&#34;&#41;&#59;
-          </Typography>
-        </Grid>
-
-        <Grid item>
           <Stack>
             <Typography fontWeight={700} fontFamily="monospace">
-              <span className={styles.coloredWord}>const</span> profile = &#123; <br />
-              &emsp; <span className={styles.coloredWord}>name:</span> "Alfred", <br />
-              &emsp; <span className={styles.coloredWord}>where:</span> "Singapore", <br />
-              &emsp; <span className={styles.coloredWord}>age:</span> {year - 2000}, <br />
-              &emsp; <span className={styles.coloredWord}>what:</span> "Full-Stack Software Engineer", <br />
-              &emsp; <span className={styles.coloredWord}>skills:</span> &#123; <br />
-              &emsp; &emsp; <span className={styles.coloredWord}>frontend:</span>[
-              {frontendSkills.map((skill) => (
-                <span>"{skill}",</span>
-              ))}
-              ]
+              <span className={styles.coloredWord}>const</span> profile = &#123;
               <br />
-              &emsp; &emsp; <span className={styles.coloredWord}>backend:</span>[
-              {backendSkills.map((skill) => (
-                <span>"{skill}", </span>
-              ))}
-              ]
+              &emsp; <span className={styles.coloredWord}>name:</span> "Alfred",
               <br />
-              &emsp; &emsp; <span className={styles.coloredWord}>devOps:</span>[
-              {devopsSkills.map((skill) => (
-                <span>"{skill}", </span>
-              ))}
-              ]
+              &emsp; <span className={styles.coloredWord}>where:</span> "Singapore",
+              <br />
+              &emsp; <span className={styles.coloredWord}>age:</span> {year - 2000}
+              <br />
+              &emsp; <span className={styles.coloredWord}>what:</span> "Full-Stack Software Engineer",
+              <br />
+              &emsp; <span className={styles.coloredWord}>skills:</span> &#123;
+              <br />
+              &emsp; &emsp; <span className={styles.coloredWord}>frontend: </span>[{frontendSkills.map((skill) => `"${skill}"`).join(", ")}]
+              <br />
+              &emsp; &emsp; <span className={styles.coloredWord}>backend: </span>[{backendSkills.map((skill) => `"${skill}"`).join(", ")}]
+              <br />
+              &emsp; &emsp; <span className={styles.coloredWord}>devOps: </span>[{devopsSkills.map((skill) => `"${skill}"`).join(", ")}]
               <br />
               &emsp; &#125;
+              <br />
+              &emsp; <span className={styles.coloredWord}>certificates: </span>[{certificate.map((skill) => `"${skill}"`).join(", ")}]
+              <br />
+              &emsp; <span className={styles.coloredWord}>learning:</span> "Cloud Technology (AWS)"
+              <br />
             </Typography>
             &#125;
           </Stack>
+        </Grid>
+
+        <Grid item>
+          <Button startIcon={<DownloadIcon />} color="inherit" variant="outlined" onClick={downloadResume}>
+            Download my CV now!
+          </Button>
         </Grid>
       </Stack>
     </Grid>
